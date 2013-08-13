da963477db88baebd20323ad60513c315c145733
José Ángel Martínez Torres <joseangel@digi21.net>
2013-08-13 18:59:56 +0200

    Nueva orden DETECTAR_LINEAS_PERIMETRO_INFERIOR

---
+++ OrdenesDigiNG/DetectaLíneasPerímetroInferiorAValor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digi21.DigiNG.Plugin.Command;
+using Digi21.Utilities;
+using Digi21.DigiNG;
+using Digi21.DigiNG.Entities;
+using Digi21.Digi3D;
+
+namespace OrdenesDigiNG
+{
+    [Command(Name="detectar_lineas_perimetro_inferior")]
+    public class DetectaLíneasPerímetroInferiorAValor : Command
+    {
+        public DetectaLíneasPerímetroInferiorAValor()
+        {
+            this.Initialize += new EventHandler(DetectaLíneasPerímetroInferiorAValor_Initialize);
+        }
+
+        void DetectaLíneasPerímetroInferiorAValor_Initialize(object sender, EventArgs e)
+        {
+            double perímetro = int.Parse(this.Args[0]);
+            List<string> códigos = new List<string>(this.Args.Skip(1));
+
+            var líneasDetectadas = from línea in DigiNG.DrawingFile.NoEliminadas().OfType<ReadOnlyLine>().QueTenganAlgúnCódigoConComodín(códigos)
+                                   where línea.Perimeter < perímetro
+                                   select línea;
+            
+            foreach (var línea in líneasDetectadas)
+            {
+                var mensaje = string.Format("Línea ({0}) con perímetro: {1} inferior a {2}", 
+                    línea.Codes[0].Name,
+                    línea.Perimeter,
+                    perímetro);
+                Digi3D.Tasks.Add(new TaskZoomEntity(línea, 2, mensaje, TaskSeverity.Error));
+            }
+        }
+    }
+}
