8143e3cd5488462562a12ec57a6ff903a478b84d
José Ángel Martínez Torres <joseangel@digi21.net>
2013-10-22 09:09:29 +0200

    Nueva orden para activar/desactivar
    Activa/desactiva las órdenes de eliminar_entidades_por_area_pequeña y
    sustituye_area_por_puntual

---
+++ OrdenesDigiNG/Modelo de datos/SustituirOEliminarEntidadesPorArea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digi21.DigiNG.Plugin.Command;
+
+namespace Ordenes.ModeloDeDatos
+{
+    [Command(Name="sustituir_o_eliminar_entidades_por_area")]
+    public class SustituirOEliminarEntidadesPorArea : Command
+    {
+        public static bool Sustituir = true;
+
+        public SustituirOEliminarEntidadesPorArea()
+        {
+            this.Initialize += new EventHandler(SustituirOEliminarEntidadesPorArea_Initialize);
+        }
+
+        void SustituirOEliminarEntidadesPorArea_Initialize(object sender, EventArgs e)
+        {
+            if (0 == this.Args.Length)
+                Sustituir = !Sustituir;
+            else
+            {
+                int valor;
+                if (int.TryParse(this.Args[0], out valor))
+                {
+                    Sustituir = 0 != valor;
+                } 
+            }
+
+            Dispose();
+        }
+    }
+}
