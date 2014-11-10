7a80e88e078a48075f8a53e95b56b10c5968d705
José Ángel Martínez Torres <joseangel@digi21.net>
2014-11-10 11:34:53 +0100

    Nueva orden para partir polígonos
    Parte múltiples polígonos por la línea de límite seleccionada.

---
+++ OrdenesDigiNG/Operaciones con entidades/PartirPoligonos.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Digi21.DigiNG.Plugin.Command;
+using Digi21.Digi3D;
+using Digi21.DigiNG.Entities;
+using Digi21.DigiNG;
+using Digi21.Math;
+using Digi21.Utilities;
+using Digi21.DigiNG.Plugin.Shell;
+
+namespace Ordenes.OperacionesConEntidades
+{
+    [Command(Name = "Partir_poligonos")]
+    [CommandInMenu("Partir polígonos", MenuItemGroup.EditGroup9Group15)]
+    public class PartirPoligonos : Command
+    {
+        public PartirPoligonos()
+        {
+            this.Initialize += new EventHandler(PartirPoligonos_Initialize);
+            this.SetFocus += new EventHandler(PartirPoligonos_SetFocus);
+            this.DataUp += new EventHandler<Digi21.Math.Point3DEventArgs>(PartirPoligonos_DataUp);
+            this.EntitySelected += new EventHandler<EntitySelectedEventArgs>(PartirPoligonos_EntitySelected);
+        }
+
+        void PartirPoligonos_EntitySelected(object sender, EntitySelectedEventArgs e)
+        {
+            try
+            {
+                ReadOnlyLine límite = e.Entity as ReadOnlyLine;
+
+                var entidadesRecortables = (from entidad in DigiNG.DrawingFile
+                                            where entidad != límite
+                                            where entidad is ReadOnlyPolygon || entidad is ReadOnlyLine && (entidad as ReadOnlyLine).Closed
+                                            where !entidad.Deleted
+                                            where entidad.AlgúnCódigoVisible()
+                                            where Window2D.Intersects(límite, entidad)
+                                            select entidad as IClippable).ToList();
+
+                List<Entity> entidadesAAñadir = new List<Entity>();
+                List<Entity> entidadesAEliminar = new List<Entity>();
+                int oldPorciento = -1;
+                for (int i = 0; i < entidadesRecortables.Count; i++)
+                {
+                    int porciento = i * 100 / entidadesRecortables.Count;
+                    if (porciento != oldPorciento)
+                    {
+                        oldPorciento = porciento;
+                        Digi3D.StatusBar.ProgressBar.Value = porciento;
+                    }
+                    try
+                    {
+                        var partes = entidadesRecortables[i].Clip(límite);
+                        if (partes.Count() > 0)
+                        {
+                            entidadesAAñadir.AddRange(partes);
+                            entidadesAEliminar.Add(entidadesRecortables[i] as Entity);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                string descripción = string.Format("Se partieron {0} entidades y se formaron {1} entidades nuevas",
+                    entidadesAEliminar.Count,
+                    entidadesAAñadir.Count);
+                Digi3D.Music(MusicType.EndOfLongProcess);
+                Digi3D.ShowBallon("Partir poligonos",
+                    descripción,
+                    2);
+
+                DigiNG.DrawingFile.Add(entidadesAAñadir);
+                DigiNG.DrawingFile.Delete(entidadesAEliminar);
+
+                if (this.Args.Length != 0 && DigiNG.DrawingFile.Contains(límite))
+                    DigiNG.DrawingFile.Delete(límite);
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+        void PartirPoligonos_DataUp(object sender, Digi21.Math.Point3DEventArgs e)
+        {
+            DigiNG.SelectEntity(e.Coordinates, entidad => entidad is ReadOnlyLine);
+        }
+
+        void PartirPoligonos_SetFocus(object sender, EventArgs e)
+        {
+            SolicitaSeleccionaEntidad();
+        }
+
+        void PartirPoligonos_Initialize(object sender, EventArgs e)
+        {
+            SolicitaSeleccionaEntidad();
+        }
+
+        void SolicitaSeleccionaEntidad()
+        {
+            Digi3D.StatusBar.Text = "Selecciona la línea de límite";
+        }
+
+    }
+}
