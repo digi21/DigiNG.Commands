8a11606ed913ba0cb44514699e9af78e3ba211ef
José Ángel <joseangelmt@outlook.com>
2023-08-22 10:30:33 +0200

    Primeros cambios para portar a .NET 7.0

--- DigiNG.Commands/Properties/AssemblyInfo.cs
+++ DigiNG.Commands/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DigiNG.Commands")]
-[assembly: AssemblyDescription("Implementa órdenes para Digi3D.NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dreaming With Objects S.L.")]
-[assembly: AssemblyProduct("DigiNG.Commands")]
-[assembly: AssemblyCopyright("Copyright © 2010-2021 José Ángel Martínez Torres")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,18 +11,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("32c90018-62ed-4e56-b000-1deb8f0a5c16")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.0.0.0")]
-[assembly: AssemblyFileVersion("22.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
 
